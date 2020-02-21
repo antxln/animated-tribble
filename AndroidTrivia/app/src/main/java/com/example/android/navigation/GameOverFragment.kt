2beacefb9dbf53215954384289d676ba22ca1216
axn9t9 <46301114+axn9t9@users.noreply.github.com>
2020-02-21 11:11:09 -0500

    Pass args from GameFragment to GameOver

--- AndroidTrivia/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -34,6 +35,9 @@ class GameOverFragment : Fragment() {
         binding.tryAgainButton.setOnClickListener { view: View ->
             view.findNavController().navigate(GameOverFragmentDirections.actionGameOverFragmentToGameFragment())
         }
+
+        var args = GameOverFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(context, "Num Correct: ${args.numCorrect} Num Questions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
         return binding.root
     }
 }
