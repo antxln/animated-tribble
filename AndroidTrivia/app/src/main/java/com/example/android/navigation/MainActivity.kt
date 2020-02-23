47a6342a90bcd442c2cf39b8196d3d4009e4a8ee
axn9t9 <46301114+axn9t9@users.noreply.github.com>
2020-02-22 23:46:51 -0500

    Lock nav drawer with navigation listener

--- AndroidTrivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
@@ -38,6 +39,13 @@ class MainActivity : AppCompatActivity() {
         if (this.supportActionBar?.isShowing == true) {
             NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
             NavigationUI.setupWithNavController(binding.navView, navController)
+            navController.addOnDestinationChangedListener{ nc: NavController, nd: NavDestination, args: Bundle? ->
+                if (nd.id == nc.graph.startDestination) {
+                    drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+                } else {
+                    drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+                }
+            }
         }
     }
 
