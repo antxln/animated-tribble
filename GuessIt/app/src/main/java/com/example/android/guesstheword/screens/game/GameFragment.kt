c87a9e15f27f99712f0d7b4f63df69c526c95e38
axn9t9 <46301114+axn9t9@users.noreply.github.com>
2020-03-01 17:46:30 -0500

    Create Observer relationship btw LiveData and UI
    LiveData allows data and UI to stay separated, and
    facilitates updating UI dependent on data.
    Estalishes an explicit and maintainable
    relationship between data and UI.

--- GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -22,6 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
@@ -49,24 +50,25 @@ class GameFragment : Fragment() {
 
         viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
 
+        viewModel.score.observe(this, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
+        viewModel.word.observe(this, Observer { newWord ->
+            binding.wordText.text = newWord
+        })
+
         binding.correctButton.setOnClickListener {
             viewModel.onCorrect()
-            updateScoreText()
-            updateWordText()
             if (viewModel.wordList.isEmpty()) {
                 gameFinished()
             }
         }
         binding.skipButton.setOnClickListener {
             viewModel.onSkip()
-            updateScoreText()
-            updateWordText()
             if (viewModel.wordList.isEmpty()) {
                 gameFinished()
             }
         }
-        updateScoreText()
-        updateWordText()
         return binding.root
 
     }
@@ -75,18 +77,7 @@ class GameFragment : Fragment() {
      * Called when the game is finished
      */
     private fun gameFinished() {
-        val action = GameFragmentDirections.actionGameToScore(viewModel.score)
+        val action = GameFragmentDirections.actionGameToScore(viewModel.score.value ?: 0)
         findNavController(this).navigate(action)
-    }
-
-    /** Methods for updating the UI **/
-
-    private fun updateWordText() {
-        binding.wordText.text = viewModel.word
-
-    }
-
-    private fun updateScoreText() {
-        binding.scoreText.text = viewModel.score.toString()
     }
 }
