02f550833f0c2aa42d79d448d970dadd8b5db5d8
axn9t9 <46301114+axn9t9@users.noreply.github.com>
2020-05-10 21:59:32 -0400

    Create Database singleton class

--- SleepTracker/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ SleepTracker/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,42 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    companion object {
+
+        // Volatile annotation indicates value is updated immediately
+        // (writes/reads are done to/from main memory) and never cached.
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context): SleepDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                // Migration strategy is for converting data of old schema
+                // to new schema, when schema changes (i.e. adding attributes)
+                // Destructive migration ignores transferring existing data
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep_history_database"
+                    )
+                            .fallbackToDestructiveMigration()
+                            .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
