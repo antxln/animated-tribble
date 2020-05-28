4986577a4526faaec0936ba7b31216d1ac8a9555
axn9t9 <46301114+axn9t9@users.noreply.github.com>
2020-05-28 18:57:39 -0400

    Create Retrofit service implementing API interface
    Retrofit builder requires base url of server and converter factory to
    convert network response to useable data. Scalars Converter converts
    JSON to String.
    Retrofit returns implementation of API interface, handling details such
    as running requests in background.

--- MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,4 +17,43 @@
 
 package com.example.android.marsrealestate.network
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
 private const val BASE_URL = "https://mars.udacity.com/"
+
+/**
+ * Retrofit builder requires base url and Converter factory for
+ * converting network response to another data form.
+ * ScalarsConverter converts JSON to String
+ */
+private val retrofit = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+/**
+ * Define annotated interface for interacting with web service.
+ * Retrofit returns an implementation of this interface, used to interact
+ * with the web service, handling details such as running requests in the
+ * background (not blocking main).
+ * Call object is used to start the request.
+ */
+interface MarsApiService {
+    @GET("realestate")
+    fun getProperties(): Call<String>
+}
+
+object MarsApi {
+    /**
+     * Retrofit create call is expensive and only requires one instance
+     * so lazy initialization is used.
+     * Initialization is run when property is first used, and future
+     * references return the same value, without re-running lazy block.
+     */
+    val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
