985495b618c907a7f5580252bc8acfadfa031cac
axn9t9 <46301114+axn9t9@users.noreply.github.com>
2020-06-01 12:57:23 -0400

    Convert Call-based API to Coroutine w/ CallAdapter

--- MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,9 +17,10 @@
 
 package com.example.android.marsrealestate.network
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Call
+import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -34,9 +35,11 @@ private val moshi = Moshi.Builder()
  * Retrofit builder requires base url and Converter factory for
  * converting network response to another data form.
  * Moshi Converter converts JSON to Kotlin objects
+ * CallAdapterFactory allows Retrofit API to return non-default Call class
  */
 private val retrofit = Retrofit.Builder()
         .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
         .baseUrl(BASE_URL)
         .build()
 
@@ -45,11 +48,11 @@ private val retrofit = Retrofit.Builder()
  * Retrofit returns an implementation of this interface, used to interact
  * with the web service, handling details such as running requests in the
  * background (not blocking main).
- * Call object is used to start the request.
+ * Deferred is a coroutine job that returns a result.
  */
 interface MarsApiService {
     @GET("realestate")
-    fun getProperties(): Call<List<MarsProperty>>
+    fun getProperties(): Deferred<List<MarsProperty>>
 }
 
 object MarsApi {
